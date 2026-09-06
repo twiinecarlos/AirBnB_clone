a422c8d5358efe2a321dd0f77e163a5aa1f547d6
twiinecarlos <99586350+twiinecarlos@users.noreply.github.com>
2026-09-06 23:59:37 +0200

    Use portable datetime parsing for BaseModel
    Change datetime parsing method for created_at and updated_at attributes.

--- models/base_model.py
+++ models/base_model.py
@@ -17,7 +17,7 @@ class BaseModel:
                 if key == "__class__":
                     continue
                 if key == "created_at" or key == "updated_at":
-                    value = datetime.fromisoformat(value)
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
