ea3b5ed96ea9adf74d20dea803cc34ec7feaa9c4
Twiine Carlos <twiinecarlos@gmail.com>
2026-09-06 11:43:53 +0200

    Create BaseModel class

---
+++ models/base_model.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Defines the BaseModel class."""
+
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    """Base class for all AirBnB objects."""
+
+    def __init__(self):
+        """Initialize a new BaseModel instance."""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """Return the string representation of the instance."""
+        return "[{}] ({}) {}".format(
+            self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        """Update the updated_at attribute."""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Return a dictionary containing the instance attributes."""
+        obj_dict = self.__dict__.copy()
+        obj_dict["__class__"] = self.__class__.__name__
+        obj_dict["created_at"] = self.created_at.isoformat()
+        obj_dict["updated_at"] = self.updated_at.isoformat()
+        return obj_dict
