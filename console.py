886b5cf8e8dfc8f203af81564a24ea64dbc2a93c
Twiine Carlos <twiinecarlos@gmail.com>
2026-09-11 12:18:49 +0200

    Update console command interpreter

--- console.py
+++ console.py
@@ -10,14 +10,18 @@ class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
 
     def do_quit(self, arg):
-        """Quit the command interpreter."""
+        """Quit command to exit the program."""
         return True
 
     def do_EOF(self, arg):
-        """Handle EOF and quit the command interpreter."""
+        """Handle EOF and quit the program."""
         print()
         return True
 
+    def emptyline(self):
+        """Do nothing when an empty line is entered."""
+        pass
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     HBNBCommand().cmdloop()
