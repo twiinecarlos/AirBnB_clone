9e0732cd17ee3097f4900b3b8bee20fca8d36b7c
Twiine Carlos <twiinecarlos@gmail.com>
2026-09-11 15:03:25 +0200

    Support User serialization and deserialization

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -35,9 +35,27 @@ class FileStorage:
             with open(FileStorage.__file_path, "r") as file:
                 objects_dict = json.load(file)
 
+            from models.amenity import Amenity
             from models.base_model import BaseModel
+            from models.city import City
+            from models.place import Place
+            from models.review import Review
+            from models.state import State
+            from models.user import User
+
+            classes = {
+                "BaseModel": BaseModel,
+                "User": User,
+                "Place": Place,
+                "State": State,
+                "City": City,
+                "Amenity": Amenity,
+                "Review": Review,
+            }
 
             for key, value in objects_dict.items():
-                FileStorage.__objects[key] = BaseModel(**value)
+                class_name = value.get("__class__")
+                if class_name in classes:
+                    FileStorage.__objects[key] = classes[class_name](**value)
         except FileNotFoundError:
             pass
